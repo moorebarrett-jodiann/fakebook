eaed40f1e320b35a8eb2e598b96a4c75f9676d9d
Jodi-Ann Moore-Barrett <moorebarrett.jodiann@gmail.com>
2022-11-28 14:15:32 -0600

    Setup Timeline Cards

--- src/scripts/Subscriber.js
+++ src/scripts/Subscriber.js
@@ -6,6 +6,41 @@
  * 
  * */
 
+import User from './User.js';
 
+class Subscriber extends User {
+
+    #pages;
+    #groups;
+    #canMonetize;
+
+    constructor(id, name, userName, email, pages, groups, canMonetize = true) {
+        super(id, name, userName, email)
+        this.#pages = pages;
+        this.#groups = groups;
+        this.#canMonetize = canMonetize;
+    }
+
+    get pages() {
+        return this.#pages;
+    }
+    
+    get groups() {
+        return this.#groups;
+    }
+    
+    get canMonetize() {
+        return this.#canMonetize;
+    }
+
+    getInfo() {
+        return `
+            ${super.getInfo()}
+            ${this.pages}
+            ${this.groups}
+            ${this.canMonetize}
+        `;
+    }
+}
 
 export default Subscriber;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,5 +7,111 @@
  * */
 
 import { select, onEvent } from './utils.js';
+import Subscriber from './Subscriber.js';
 
 /**--------------------------------- Data ----------------------------------- */
+const form = select('form');
+const gridBox = select('.grid');
+const postInput = select('.post-input');
+const postFile = select('.post-file');
+const fileNameSpan = select('.file-name-selected');
+const createPost = select('.create-post');
+const message = select('.message');
+
+/**-------------------------------------------------------------------------- */
+
+/**-----------------Timeline Functions and Events---------------------------- */
+
+// submit form
+function submitForm(postInput, postFile) {
+
+    try {
+        window.URL = window.URL || window.webkitURL;
+        let url = URL.createObjectURL(postFile.files[0]);
+        let image = select('.avatar').innerHTML;
+        let today = new Date();
+        const subscriber = new Subscriber(1, 'Jodi-Ann Barrett', 'jbarrett', 'jbarrett@email.com');
+
+        // build grid children
+        var div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+            <div class="post-header flexbox">
+                ${image}
+                <h4>${subscriber.name}</h4>
+                <p>${today.toDateString()}</p>
+            </div>
+            <div class="post-body">
+                <p>${postInput.value}</p>
+                <img src="${url}"/>
+            </div>
+        `;
+        gridBox.prepend(div);
+
+    } catch (error) {
+        message.innerHTML = `<p class="invalid">${error}</p>`;
+    }
+}
+
+// function to validate form input
+function validateFormInput () {
+    // console.log(postInput.value);
+    // console.log(postFile.value);
+    if(postInput.value === '' && postFile.value === '') {
+        message.innerHTML = `<p class="invalid">Your post cannot be empty.</p>`;
+    } else {
+        submitForm(postInput, postFile);
+        postInput.value = '';
+        postFile.value = '';
+        fileNameSpan.innerText = '';
+    }
+}
+
+// validate form when add button is clicked
+onEvent('click', createPost, function (event) {
+    event.preventDefault();
+    validateFormInput();
+});
+
+// when page is reloaded clear grid and form
+onEvent('load', window, () => {
+    gridBox.innerHTML = '';
+    fileNameSpan.innerText = '';
+    form.reset();
+});
+
+// when file is selected show the file name to the user
+onEvent('change', postFile, function() {
+    // extract name of the file:
+    fileNameSpan.innerText = this.files[0].name.trim();
+});
+
+/**-------------------------------------------------------------------------- */
+
+/**-------------------------Subscriber Profile------------------------------- */
+
+const profilePhoto = select('.avatar');
+const overlay = document.querySelector('.overlay');
+
+contactBtn.addEventListener('click', () => {
+  overlay.removeAttribute('style', `
+    visibility: hidden;
+  `);
+  overlay.setAttribute('style', `
+    visibility: visible;
+  `);
+});
+
+overlay.addEventListener('click', function (){
+  this.removeAttribute('style', `
+    visibility: visible;
+  `);
+  this.setAttribute('style', `
+    visibility: hidden;
+  `);
+});
+
+
+
+
+/**-------------------------------------------------------------------------- */
